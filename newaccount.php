d3acb1bc79e2f42b088f0acac5a351135ec06dc8
Gvidas <gvisis@gmail.com>
2021-03-03 00:07:38 +0200

    alert message same persid found

--- newaccount.php
+++ newaccount.php
@@ -1,3 +1,5 @@
+<!-- Forma resubmitinasi refreshinant -->
+
 <?php 
 require_once __DIR__.'/header.php';
 require __DIR__.'/accFile.php';
@@ -8,56 +10,48 @@ $lastname = "";
 $accNumb = "";
 $personalID = '';
 $accbalance = 0;
-echo '<pre>';
-var_dump(array_column($accUsers, 'persid'));
-echo '</pre>';
 
-// if (isset($_SESSION['msg_status']) && $_SESSION['msg_status'] === 0) {
-//   _d($_POST);
-//   $firstname = $_POST['firstname'];
-//   $lastname =$_POST['lastname'];
-//   $accNumb = $_POST['accNumb'];
-//   $personalID = $_POST['persid'];
-//   $accountFile = __DIR__.'/accounts.json';
-//   _d($_SESSION['msg']);
-//   session_destroy();
-// }
+$accountFile = __DIR__.'/accounts.json';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   
   $personIdExist = array_search($_POST['persid'],array_column($accUsers, 'persid'));
-  _d(!$personIdExist);
-
-  if (!$personIdExist){
+  
+  if ($personIdExist){
     $_SESSION['msg'] = 'User with the same personal ID already exists';
     $_SESSION['msg_status'] = 0;
     $firstname = $_POST['firstname'];
     $lastname =$_POST['lastname'];
     $accNumb = $_POST['accNumb'];
-    session_destroy();
+  } else {
+    $_POST['accbalance'] = $accbalance;
+    
+    // pasiimam is failo
+    $accountsJSON= file_get_contents($accountFile);
+    // atkodinam kaip array
+    $accountsArr = json_decode($accountsJSON, true);
+    // ikeliam visa post i iskoduota array
+    $accountsArr[uniqid()] = $_POST;
+
+    // uzkoduojam is naujo array
+    $writeNewAccount= json_encode($accountsArr);
+    
+    $_SESSION['msg'] = $_POST['firstname'] . " " . $_POST['lastname'] ." was succesffuly added to the list!";
+    $_SESSION['msg_status'] = 1;
+    
+    // idedam vel i faila
+    file_put_contents($accountFile,$writeNewAccount);
+    header("Location: ./index.php");
+    exit;
   }
-
-  $_POST['accbalance'] = $accbalance;
-  
-  // pasiimam is failo
-  $accountsJSON= file_get_contents($accountFile);
-  // atkodinam kaip array
-  $accountsArr = json_decode($accountsJSON, true);
-  // ikeliam visa post i iskoduota array
-  $accountsArr[uniqid()] = $_POST;
-
-  // uzkoduojam is naujo array
-  $writeNewAccount= json_encode($accountsArr);
-  
-  $_SESSION['msg'] = $_POST['firstname'] . " " . $_POST['lastname'] ." was succesffuly added to the list!";
-  $_SESSION['msg_status'] = 1;
-  
-  // idedam vel i faila
-  file_put_contents($accountFile,$writeNewAccount);
-   header("Location: ./index.php");
-   exit;
 }
 ?>
+
+<?php if (isset($_SESSION['msg_status']) && $_SESSION['msg_status'] === 0) : ?>
+  <div class="alert alert-<?= ($_SESSION['msg_status']) == 0 ? 'danger' : 'success'?>" role="alert">
+    <?= $_SESSION['msg']; session_destroy();?>
+  </div>  
+<?php endif; ?>
 
 <form action="" method="post">
   <div class="mb-3">
